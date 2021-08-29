245589d34ce574c8d0156aa1516be745b3825ddd
Lukasz Wiecaszek <lukasz.wiecaszek@gmail.com>
2021-08-29 13:11:08 +0200

    Move magnitude_stats to separate file

--- lib/acquisition_and_tracking_impl.h
+++ lib/acquisition_and_tracking_impl.h
@@ -26,6 +26,7 @@
 #include <memory>
 #include <array>
 
+#include "magnitude_stats.h"
 #include "doppler_shifts.h"
 #include "dll_loop_filter.h"
 #include "pll_loop_filter.h"
@@ -42,68 +43,6 @@ namespace gr {
     constexpr int correlation_taps = 3;
     constexpr double correlation_shift = 0.25;
     constexpr double correlation_shifts[correlation_taps] = {-correlation_shift, 0, +correlation_shift};
-
-    template<typename T>
-    struct magnitude_stats
-    {
-      magnitude_stats(int freq_index = -1)
-      {
-        reset(freq_index);
-      }
-
-      void reset(int freq_index = -1)
-      {
-        d_freq_index = freq_index;
-        d_max_index = -1;
-        d_min = 0.0;
-        d_max = 0.0;
-        d_avg = 0.0;
-      }
-
-      std::string to_string() const
-      {
-        char buf[128];
-
-        snprintf(buf, sizeof(buf), "idx: %d, freq_idx: %d, min: %e, max: %e, avg: %e",
-          d_max_index, d_freq_index, d_min, d_max, d_avg);
-
-        return std::string(buf);
-      }
-
-      static magnitude_stats get(const std::vector<T>& v, int freq_index)
-      {
-        magnitude_stats stats{freq_index};
-
-        if (v.size() > 0) {
-          stats.d_max_index = 0;
-          stats.d_min = stats.d_max = stats.d_avg = v[0];
-
-          for (int i = 1; i < v.size(); ++i) {
-            if (v[i] < stats.d_min) {
-              stats.d_min = v[i];
-            }
-            if (v[i] > stats.d_max) {
-              stats.d_max = v[i];
-              stats.d_max_index = i;
-            }
-            stats.d_avg += v[i];
-          }
-
-          if (v.size() > 1) {
-            stats.d_avg -= stats.d_max;
-            stats.d_avg /= (v.size() - 1);
-          }
-        }
-
-        return stats;
-      }
-
-      int d_freq_index;
-      int d_max_index;
-      T d_min;
-      T d_max;
-      T d_avg;
-    };
 
     template<typename ITYPE0, typename OTYPE0>
     class acquisition_and_tracking_impl : public acquisition_and_tracking
